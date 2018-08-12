0233b33399485ed3daf254351e37af0f411601fd
Hugo Martinez <hugomartinezdev@hotmail.com>
2018-08-12 18:47:20 +0200

    feat(app): add firebase

--- containers/App/App.spec.tsx
+++ containers/App/App.spec.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { config } from './constants';
+import firebase from 'firebase/app';
 import App from './App';
 
 let wrapper;
+let spy;
 
 const setup = () =>
   shallow(
@@ -12,8 +15,22 @@ const setup = () =>
   );
 
 describe('<App />', () => {
-  test('should render properly', () => {
+  beforeAll(() => {
+    spy = jest.spyOn(firebase, 'initializeApp').mockImplementation(() => {});
     wrapper = setup();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('should render properly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('componentDidMount', () => {
+    test('it should configure firebase properly', () => {
+      expect(spy).toHaveBeenCalledWith(config);
+    });
   });
 });
--- containers/App/App.tsx
+++ containers/App/App.tsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Header from '@components/Header';
 import colors from '../../constants/colors';
+import firebase from 'firebase/app';
+import { config } from './constants';
 
 interface PropsType {
   children: JSX.Element[] | JSX.Element;
 }
 class App extends Component<PropsType, {}> {
+  componentDidMount() {
+    firebase.initializeApp(config);
+  }
+
   render() {
     const { children } = this.props;
-
     return (
       <div className="page">
         <Header />
