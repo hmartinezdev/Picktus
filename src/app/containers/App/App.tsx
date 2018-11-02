3ffac20aa2a4ddf2118327e609ded73f157541eb
Hugo Martinez <hugomartinezdev@hotmail.com>
2018-11-02 23:25:39 +0100

    feat(classicauth): classic auth working

--- src/app/containers/App/App.tsx
+++ src/app/containers/App/App.tsx
@@ -11,6 +11,8 @@ class App extends Component<IAppPropsType> {
   constructor(props: IAppPropsType) {
     super(props);
 
+    console.log(firebase);
+
     // If the firebase setup has not been done yet
     // we do it here
     if (!firebase.apps.length) {
